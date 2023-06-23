9fa29524bdbe26c54355f6a36c69fae5a4a6ba7e
saanvi <95546881+1789038@users.noreply.github.com>
2023-06-23 13:55:40 -0400

    Add files via upload

---
+++ chewinggum.js
@@ -0,0 +1,36 @@
+img="";
+status="";
+function preload(){
+    img=loadImage('chewing_gum.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.position(400,200);
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("Chewing Gum",45,75);
+    noFill();
+    stroke("#FF0000");
+    rect(30,60,450,350);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function home(){
+    window.location="index.html";
+}
